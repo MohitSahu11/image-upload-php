9301fc48e6c20111c0a43b7a63308dd79f7d2fea
MohitSahu11 <86914365+MohitSahu11@users.noreply.github.com>
2023-07-05 14:07:08 +0530

    Update insert_form.php

--- insert_form.php
+++ insert_form.php
@@ -1,1 +1,18 @@
 
+<?php
+
+include 'upload_image.php';
+
+
+$image = insert_image('image','user_images','inside');
+// image file name 
+//user_image floder name
+// inside folder outside change outside ../ 
+
+// $update_image = update_image('payment_receipt','user_images','inside');
+// // ex. 
+
+
+
+
+?>
